7188689f5154b570febebd71ab8cac5d28ca54f7
Stefano Zaghi <stefano.zaghi@gmail.com>
2015-09-29 17:24:59 +0200

    Add README for errors analysis
    Add README for errors analysis

--- papers/announcement/errors_analysis/errors_analysis.sh
+++ papers/announcement/errors_analysis/errors_analysis.sh
@@ -9,23 +9,17 @@ rm -rf results-oscillation
 mkdir results-oscillation
 cd results-oscillation
 ../tests/oscillation --errors_analysis --output errors_analysis-oscillation > summary.log
-ln -fs ../utilities-oscillation/*lay .
-ln -fs ../utilities/lay_export* .
-lay_export_all_f errors_analysis-oscillation-adams-bashforth-2.lay
-lay_export_all_f errors_analysis-oscillation-adams-bashforth-3.lay
-lay_export_all_f errors_analysis-oscillation-leapfrog.lay
-lay_export_all_f errors_analysis-oscillation-ls-runge-kutta-5.lay
-lay_export_all_f errors_analysis-oscillation-tvd-runge-kutta-2.lay
-lay_export_all_f errors_analysis-oscillation-tvd-runge-kutta-3.lay
-lay_export_all_f errors_analysis-oscillation-tvd-runge-kutta-5.lay
+tecplot=$(which tec360)
+if [ -x "$tecplot" ] ; then
+  ln -fs ../utilities-oscillation/*lay .
+  ln -fs ../utilities/lay_export* .
+  ./lay_export_all_f errors_analysis-oscillation-adams-bashforth-2.lay
+  ./lay_export_all_f errors_analysis-oscillation-adams-bashforth-3.lay
+  ./lay_export_all_f errors_analysis-oscillation-leapfrog.lay
+  ./lay_export_all_f errors_analysis-oscillation-ls-runge-kutta-5.lay
+  ./lay_export_all_f errors_analysis-oscillation-tvd-runge-kutta-2.lay
+  ./lay_export_all_f errors_analysis-oscillation-tvd-runge-kutta-3.lay
+  ./lay_export_all_f errors_analysis-oscillation-tvd-runge-kutta-5.lay
+fi
 cd -
-echo "Lorenz test errors analysis"
-rm -rf results-lorenz
-mkdir results-lorenz
-echo "Burgers test errors analysis"
-rm -rf results-burgers
-mkdir results-burgers
-echo "1D Euler test errors analysis"
-rm -rf results-euler-1D
-mkdir results-euler-1D
 exit 0
